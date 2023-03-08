2364c50d22229f5ae6737dfa6eb329527387453c
CapucineWebtech <capucine.madoulaud@next-u.fr>
2023-03-08 10:38:00 +0100

    read devis

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Entity\Devis;
 use App\Entity\WineGame;
+use App\Repository\ContactRepository;
+use App\Repository\DevisRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class UserController extends AbstractController
 {
@@ -62,5 +69,68 @@ class UserController extends AbstractController
         }
 
         return $this->render('wineGame.html.twig');
+    }
+
+    #[Route('/admin', name: 'app_admin')]
+    public function admin(ContactRepository $contactRepository, DevisRepository $devisRepository): Response
+    {
+        if (!($this->isGranted('ROLE_ADMIN'))) {
+            return $this->redirectToRoute('app_compte');
+        }
+
+        $contacts = $contactRepository->findAll();
+        $deviss = $devisRepository->findAll();
+
+        return $this->render('admin.html.twig', [
+            'contacts' => $contacts,
+            'deviss' => $deviss
+        ]);
+    }
+
+    #[Route('/devis/{id}', name: 'app_devisRead')]
+    public function devis(Devis $devis): Response
+    {
+        if (!($this->isGranted('ROLE_ADMIN'))) {
+            return $this->redirectToRoute('app_compte');
+        }
+        return $this->render('devis-read.html.twig', [
+            'devis' => $devis
+        ]);
+    }
+
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/deleteContact/{id}', name: 'app_deleteContact', methods: ['POST'])]
+    public function deleteTask(Contact $contact, Request $request) : RedirectResponse
+    {
+        $submittedToken = $request->request->get('token');
+
+        if ($this->isCsrfTokenValid('delete-contact'.$contact->getId(), $submittedToken)) {
+            $this->em->remove($contact);
+            $this->em->flush();
+        }
+
+        $this->addFlash(
+            'successDelete',
+            "Le message à bien été supprimée."
+        );
+        return $this->redirectToRoute('app_admin');
+    }
+
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/deleteDevis/{id}', name: 'app_deleteDevis', methods: ['POST'])]
+    public function deleteDevis(Devis $devis, Request $request) : RedirectResponse
+    {
+        $submittedToken = $request->request->get('token');
+
+        if ($this->isCsrfTokenValid('delete-devis'.$devis->getId(), $submittedToken)) {
+            $this->em->remove($devis);
+            $this->em->flush();
+        }
+
+        $this->addFlash(
+            'successDelete',
+            "Le devis à bien été supprimée."
+        );
+        return $this->redirectToRoute('app_admin');
     }
 }
